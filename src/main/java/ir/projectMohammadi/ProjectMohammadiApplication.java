639fb18c04ae737e00597b3ba3fdfe9c7e203068
mohammad <mohammad890mohammad890@gmail.com>
2025-02-14 23:13:41 +0330

    (start project)

---
+++ src/main/java/ir/projectMohammadi/ProjectMohammadiApplication.java
@@ -0,0 +1,15 @@
+package ir.projectMohammadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class ProjectMohammadiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMohammadiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/projectMohammadi/ProjectMohammadiApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.projectMohammadi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMohammadiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
