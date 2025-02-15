18cce0e348af5da256801c8c17ae5e6593ee9b5a
mohammad <mohammad890mohammad890@gmail.com>
2025-02-15 23:33:42 +0330

    Add ModelMapper (Faz 1)

---
+++ src/main/java/ir/projectMohammadi/util/QueryResult/QueryResult.java
@@ -0,0 +1,32 @@
+package ir.projectMohammadi.util.QueryResult;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+public class QueryResult<T> {
+
+    private int pageNumber;
+
+    private int totalRecords;
+
+    private int pageSize;
+
+    @Getter
+    private List<T> entityList = new ArrayList<T>();
+
+    public QueryResult() {
+    }
+
+    public QueryResult(int pageNumber, int totalRecords, int pageSize, List<T> entityList) {
+        this.pageNumber = pageNumber;
+        this.totalRecords = totalRecords;
+        this.pageSize = pageSize;
+        this.entityList = entityList;
+    }
+
+}
