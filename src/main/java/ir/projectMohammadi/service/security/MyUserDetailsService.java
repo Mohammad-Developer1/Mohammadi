d2ba586262b7d28c81dbbb790cb61f2b1c7a2fc3
mohammad <mohammad890mohammad890@gmail.com>
2025-02-25 16:36:10 +0330

    Update MyUserDetailsService (faz 2)

--- src/main/java/ir/projectMohammadi/service/security/MyUserDetailsService.java
+++ src/main/java/ir/projectMohammadi/service/security/MyUserDetailsService.java
@@ -4,12 +4,13 @@ package ir.projectMohammadi.service.security;
 import ir.projectMohammadi.model.user.User;
 import ir.projectMohammadi.repository.User.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -18,19 +19,18 @@ public class MyUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> user = userRepository.findByUsername(username);
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        if (user.isEmpty()) {
-            throw new UsernameNotFoundException("User not found: " + username);
-        }
-
-        return org.springframework.security.core.userdetails.User
-                .withUsername(user.get().getUsername())
-                .password(user.get().getPassword())
-                .roles(user.get().getRole().name())
-                .build();
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
     }
+
 }
 
